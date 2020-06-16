85b52e5bdc7e937ebaca3d670dd32da10cf65279
Martinhw-u <66851498+Martinhw-u@users.noreply.github.com>
2020-06-15 20:42:19 -0400

    Update TDALista.c

--- TDALista.c
+++ TDALista.c
@@ -1,8 +1,7 @@
-   float promedio(Lista *lista){
-	int i, largo, suma=0;
-  largo=Fin(lista);
-  
-	for(i=0;i<Fin(lista);i++){
+  float promedio(Lista *lista){
+	  int i, largo, suma=0;
+	  largo=Fin(lista);
+	  for(i=0;i<Fin(lista);i++){
 		suma+=Recuperar(i,lista);
 	}
 
